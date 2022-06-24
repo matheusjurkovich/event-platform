249b72cc36b972cb0033410508e1b61df18bc3e8
Matheus Jurkovich <77711064+matheusjurkovich@users.noreply.github.com>
2022-06-23 21:20:03 -0300

    Aula 04 Concluida

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o1s6w104d601xr07wybzww/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 })
